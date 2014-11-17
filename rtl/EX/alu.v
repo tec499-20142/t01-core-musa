950116cd89eae81e5fff0dad24ce4fdaf1cfb4d7
lasilva <lucas.almeida.las@gmail.com>
2014-11-17 09:38:18 -0300

    Modificações na alu. Adicionado o parametro para a subtração imediata. Modificado a entrada que será utilizada para a operação not. Adicionado as saídas de overflow e underflow.

--- rtl/EX/alu.v
+++ rtl/EX/alu.v
@@ -4,23 +4,24 @@ input [31:0] data_b,
 input [4:0] alu_control, 
 input reset, 
 output reg[31:0] result,
+output reg overflow, 
+output reg underflow, 
 output reg [3:0] flag);
 
-	parameter ADD = 6'b000000;
-	parameter ADDI = 6'b000001;
-	parameter SUB = 6'b000010;
-	parameter MUL = 6'b000011; 
-	parameter DIV = 6'b000100;
-	parameter AND = 6'b000101;
-	parameter ANDI = 6'b000110;
-	parameter OR = 6'b000111;
-	parameter ORI = 6'b001000;
-	parameter NOT = 6'b001001; 
-	parameter CMP = 6'b001010;
-	parameter SLT = 6'b001011;
-	parameter BEQ = 6'b001100;
-	parameter BNE = 6'b001101;
+	parameter ADD = 0;
+	parameter ADDI = 1;
+	parameter SUB = 2;
+	parameter SUBI = 3;
+	parameter MUL = 4; 
+	parameter DIV = 5;
+	parameter AND = 6;
+	parameter ANDI = 7;
+	parameter OR = 8;
+	parameter ORI = 9;
+	parameter NOT = 10; 
+	parameter CMP = 11;
 	
+	reg [32:0] result_checker; 
 
 //ainda falta colocar algumas das funçoes aqui na alu. 
 always @(alu_control, data_a, data_b, reset)
@@ -32,16 +33,19 @@ always @(alu_control, data_a, data_b, reset)
 		else 
 			begin 
 				case(alu_control)
-					ADD: result = data_a + data_b; 
+					ADD: begin 
+						result = data_a + data_b;
+					end 
 					ADDI: result = data_a + data_b;
 					SUB: result = data_a - data_b; 
+					SUBI: result = data_a - data_b;
 					MUL: result = data_a * data_b; // isso talvez nao seja sintetizavel 
 					DIV: result = data_a / data_b;
 					AND: result = data_a & data_b; 
 					ANDI: result = data_a & data_b; 
 					OR: result = data_a | data_b; 
 					ORI: result = data_a | data_b;
-					NOT : result = ~data_b; 
+					NOT : result = ~data_a; //considerando que o registrador de destino e data_a
 					CMP : begin //isso daqui eu tenho duvida
 						if(data_a == data_b)begin 
 							result = 1;
