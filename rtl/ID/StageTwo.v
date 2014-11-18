c705e4dd8d7e89fbcd6d23e0e27a1ab347ad27a0
Patrick <patrickecomp@gmail.com>
2014-11-18 13:19:19 -0200

    Adição de blocos e multiplexadores no stagio 2. patrickecomp

--- rtl/ID/StageTwo.v
+++ rtl/ID/StageTwo.v
@@ -1,202 +1,71 @@
 module stagetwo(
-instruction, 
-writeData, writeRegister, clk, rst,  
-branch, memRead, memToReg, memWrite, aluSrc, regWrite, jump, regDst, 
+instruction, clk, rst,  
+branch, memRead, memToReg, memWrite, aluSrc, regWrite, jump, PCWrite, 
 aluOp,
 word,
 readData1, readData2, readData3, outputWord);
 
 input [31:0] instruction;
-input writeData, writeRegister, clk, rst;
-output  branch, memRead, memToReg, memWrite, aluSrc, regWrite, jump, regDst;
+input clk, rst;
+input [31:0] AluOut;
+output  branch, memRead, memWrite, aluSrc, jump, PCWrite;
 output  [1:0] aluOp;
 output  [15:0] word;
 output [31:0] readData1, readData2, readData3, outputWord;
-reg [2:0] stage = 0;
-wire [5:0] operation = instruction[31:26]; 
-reg  branch, memRead, memToReg, memWrite, aluSrc, regWrite, jump, regDst;
-reg  [1:0] aluOp;
+wire [5:0] opcode = instruction[31:26]; 
+wire [4:0] ReadRegister1 = instruction[25:21];
+wire [4:0] ReadRegister2 = instruction[20:16];
+wire MemToReg;
+wire regDst;
+wire _regWrite;
+wire [31:0] out_Mux_Write_Data;
+wire [4:0] out_destination;
+input [31:0] mem_Data;
+wire [4:0] destination = instruction[15:11];
 
-
-always@ (posedge clk or posedge rst)
+always (*)
 begin
-	if(clk)
-	begin
-		if(stage == 3'd0) begin
-		
-			stage <= 3'd1; 
-		end
-		else if (stage == 3'd1) begin
-			if(operation == 6'h01)
-				begin
-				regDst <= 1'b1;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b0;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b0;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-				end
-			else if(operation == 6'h02)
-				begin
-				regDst <= 1'b1;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b0;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b0;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-				end
-			else if(operation == 6'h03)
-				begin
-				regDst <= 1'b0;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-				end
-			else if (operation == 6'h04)
-			   begin
-			   regDst <= 1'b0;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-			   end
-			else if (operation == 6'h05)
-			   begin
-			  regDst <= 1'b0;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-			   end
-			else if (operation == 6'h06)
-			   begin
-			     regDst <= 1'b0;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b10;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-			   end
-			else if (operation == 6'h07)
-			  begin 
-			   regDst <= 1'b1;
-				branch <= 1'b0;
-				memRead <= 1'b1;
-				memToReg <= 1'b1;
-				aluOp <= 2'b00;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b1;
-				jump <= 1'b0;
-			  end
-			  
-			else if (operation == 6'h08)
-			  begin 
-			     regDst <= 1'b1;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b00;
-				memWrite <= 1'b1;
-				aluSrc <= 1'b1;
-				regWrite <= 1'b0;
-				jump <= 1'b0;
-			  end
-			else if (operation == 6'h09)
-			  begin 
-			  jump <= 1'b0;
-			  end 
-			  
-			else if (operation == 6'h0a)
-			  begin 
-			  jump <= 1'b0;
-			  end 
-			else if (operation == 6'h0b)
-			  begin 
-			      regDst <= 1'b1;
-				branch <= 1'b1;
-				memRead <= 1'b0;
-				memToReg <= 1'b1;
-				aluOp <= 2'b01;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b0;
-				regWrite <= 1'b0;
-				jump <= 1'b0;
-			  end
-			else if (operation == 6'h0c)
-			  begin 
-			  jump <= 1'b1;
-			  end
-			else if (operation == 6'h0d)
-			  begin 
-			  jump <= 1'b1;
-			  end
-			  
-			 else if (operation == 6'h0e)
-			  begin 
-			  
-			  end
-			 else if(operation == 6'h00)
-			   begin
-			     	      regDst <= 1'b0;
-				branch <= 1'b0;
-				memRead <= 1'b0;
-				memToReg <= 1'b0;
-				aluOp <= 2'b00;
-				memWrite <= 1'b0;
-				aluSrc <= 1'b0;
-				regWrite <= 1'b0;
-				jump <= 1'b0;
-			     end
-			  else
-			    begin
-			    //tratamento de erros;
-			    end
-
-
-			stage <= 3'd2;
-		end
-		else if (stage == 3'd2) begin
-		
-			stage <= 3'd3;
-		end
-		else if(stage == 3'd3) begin
-		
-			stage <= 3'd4;
-		end
-		else begin
-		
-			stage <= 3'd0;
-		end
-	end
-	else begin
-	  	stage <= 3'd0;
-	  end
-
+	if(MemToReg)
+		out_Mux_Write_Data = mem_Data;
+	else
+		out_Mux_Write_Data = AluOut;
 end
 
-endmodule
+always (*)
+begin
+	if(regDst)
+		out_destination = destination;
+	else
+		out_destination = ReadRegister2;
+end
 
+RegisterFile BLOCO1 (
+  .ReadRegister1 (ReadRegister1),
+  .clk (clk),
+  .reset (rst),
+  .ReadRegister2 (ReadRegister2),
+  .WriteData (out_Mux_Write_Data),
+  .WriteRegister(out_destination),
+  .ReadData1(readData1),
+  .ReadData2(readData2),
+  .RegWrite (_regWrite)
+  );
+  
+ unit_control BLOCO2 (
+  .opcode (ReadRegister1),
+  .clk (clk),
+  .reset (rst),
+  .branch (branch),
+  .memRead (memRead),
+  .memToReg(memToReg),
+  .memWrite(memWrite),
+  .aluSrc(aluSrc),
+  .RegWrite (_regWrite),
+  .jump (jump),
+  .RegDst (regDst),
+  .PCWrite (PCWrite),
+  .aluOp (aluOp)
+  );
 
+ endmodule
+ 
