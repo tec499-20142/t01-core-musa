0e5d566051ae92f2a54bb1fe661e3731d1e7af4b
Patrick <patrickecomp@gmail.com>
2014-11-18 22:01:50 -0200

    Correção no banco de registradores, patrickecomp

--- rtl/ID/RegisterFile.v
+++ rtl/ID/RegisterFile.v
@@ -1,6 +1,3 @@
-
-`timescale 1 ns /1 ns
-
 module RegisterFile (ReadRegister1, clk, reset, ReadRegister2, WriteData, WriteRegister, ReadData1, ReadData2, RegWrite);
 
 input [4:0] ReadRegister1;
@@ -8,7 +5,7 @@ input [4:0] ReadRegister2;
 input clk, RegWrite, reset;
 input [4:0] WriteRegister;
 input [31:0] WriteData;
-output [31:0] ReadData1, ReadData2;
+output reg [31:0] ReadData1, ReadData2;
 
 reg [31:0] MemoryFile [0:31];
 
