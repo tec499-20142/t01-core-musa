2d7c2558ed708c36f8a4c9e838cb9b130f43f608
lasilva <lucas.almeida.las@gmail.com>
2014-11-23 21:43:24 -0300

    Modificado todos os parametros para os valores de aluOp e function definidos. Realização das operações aritméticas e lógicas e as operações imediatas.

--- rtl/EX/alu.v
+++ rtl/EX/alu.v
@@ -1,5 +1,4 @@
 module alu(
-	
 	input[31:0] data_a, 
 	input [31:0] data_b,
 	input [2:0] alu_control,
@@ -9,8 +8,7 @@ module alu(
 	output reg [2:0] flag);
 
 	//Valores do aluOp 
-	parameter ARITHMETIC_LOGIC = 010; 
-	
+	parameter TYPE_R = 010; 
 	
 	//functions das operaçoes logicas e aritmeticas (instruçoes do tipo R)
 	parameter ADD = 100000;
@@ -20,6 +18,7 @@ module alu(
 	parameter AND = 100100;
 	parameter OR = 100101;
 	parameter NOT = 100111;
+	parameter CMP = 101010;
 	
 	//Opcode das instrucoes 
 	parameter ADDI = 000;
@@ -27,21 +26,34 @@ module alu(
 	parameter ANDI = 011;
 	parameter ORI = 100;
 	
-	parameter CMP = 101010;
 	parameter BRFL = 000100;
+	
+	//flags 
+	parameter FLAG_NOT_ACTIVED = 000;
+	parameter FLAG_EQUAL = 001; 
+	parameter FLAG_EXCEPTION = 010; 
+	parameter FLAG_OVERFLOW = 011;
+	parameter FLAG_UNDERFLOW = 100;
+	parameter FLAG_ABOVE = 101;
 	
 	reg [2:0] reg_flag;//registrador de flag
 	reg [32:0] result_checker; 
 	reg [64:0] result_muld;
 
 //ainda falta colocar algumas das funçoes aqui na alu. 
-always @(alu_control, func, data_a, data_b, reset) begin
+always @(alu_control, func, data_a, data_b, reset) 
 		if(reset) begin
-				result = 32'b0;
-			end
+			result = 0;
+		end
 		else	begin 
 			case (alu_control)
-				ARITHMETIC_LOGIC: begin 
+				ADDI: begin
+					result_checker = data_a + data_b;
+				end 
+				SUBI: begin 
+					result_checker = data_a - data_b;
+				end 
+				TYPE_R: begin 
 					case(func)
 						ADD: begin 
 							result_checker = data_a + data_b;
@@ -55,7 +67,23 @@ always @(alu_control, func, data_a, data_b, reset) begin
 						DIV: begin 
 							result_muld = data_a / data_b;
 						end 
+						CMP: begin 
+							if(data_a == data_b) begin 
+								flag = FLAG_EQUAL;
+							end else begin
+								if(data_a > data_b) begin 
+									flag = FLAG_ABOVE;
+								end 
+							end 
+						end
 					endcase
-				end		
-			endcase 
+				end
+				ANDI: begin 
+					result = data_a & data_b;
+				end 
+				ORI: begin 
+					result = data_a | data_b;
+				end 
+			endcase
+		end 	
 endmodule 
