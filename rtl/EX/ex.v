6af4ff3c18059e36e1d5463b958b08f7bf3de479
lasilva <lucas.almeida.las@gmail.com>
2014-12-08 13:29:40 -0300

    Alteração do estágio EX para as novas adaptações dos valores de controle

--- rtl/EX/ex.v
+++ rtl/EX/ex.v
@@ -17,6 +17,40 @@ module ex_stage(
 	output reg[31:0] next_pc, 
 	); 
 
+	reg data_a_temp;
+	reg data_b_temp;
 	
+	mux m_data_a(
+	.data_a(pc_1),
+	.data_b(data_a), 
+	.sel(data_a_select), 
+	.data(data_a_temp));
+	
+	mux m_data_b(
+	.data_a(data_b),
+	.data_b(immediate), 
+	.sel(data_b_select), 
+	.data(data_b_temp));
+	
+	alu alu_ex(
+	.reset(reset), 
+	.data_a(data_a_temp), 
+	.data_b(data_b_temp),
+	.alu_control(alu_control),
+	.func(func),  
+	.result(result), 
+	.flag(flag));
+
+	cal_next_address calc(
+	.reset(reset),
+	.pc(pc),
+	.pc_1(pc_1), 
+	.jump_address(jump_address), 
+	.branch_address(result), 
+	.stack(stack), 
+	.jr(data_a),
+	.pc_select(pc_select),
+	.next_address(next_pc)
+);
 	
 endmodule 
