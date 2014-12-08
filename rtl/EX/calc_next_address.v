7b0b4864a7c8bbbc5591de2dea62160788d14eff
lasilva <lucas.almeida.las@gmail.com>
2014-12-08 12:49:23 -0300

    Altera do calculo do próximo endereço

--- rtl/EX/calc_next_address.v
+++ rtl/EX/calc_next_address.v
@@ -1,46 +1,46 @@
 module cal_next_address(
-	input [31:0] pc_in,
-	input[31:0] brfl_address,
-	input [31:0] jump_address,
-	input branch, 
-	input jump_sel,
-	input [3:0] alu_flag,
 	input reset,
+	input [31:0] pc,
+	input [31:0] pc_1, 
+	input [31:0] jump_address, 
+	input [31:0] branch_address, 
+	input [31:0] stack, 
+	input [31:0] jr,
+	input [2:0] pc_select,
 	output reg [31:0] next_address
 );
 
-reg[31:0] next_temp;
+	parameter STACK = 3'b000; 
+	parameter JR = 3'b001; 
+	parameter NPC = 3'b010; 
+	parameter JUMP = 3'b011; 
+	parameter BRANCH = 3'b100; 
+	parameter HALT = 3'b101;
+	
 
 always @(*)begin 
-		// se for reset, zera o reg temporário
-		if(reset) begin
-			next_temp = 0;
+	case(pc_select)
+		STACK: begin 
+			next_address = stack; 
 		end 
-		else begin
-			// se for brfl o endereço q vem em data_a é associado ao reg temporário
-			if(branch && alu_flag == data_b)begin
-				next_temp = data_a; 
-			end 
-			else begin
-				// se for jump, coloca endereço do jump no reg temporário
-				if(jump_sel) begin
-					next_temp = jump_address; 
-				end
-				// normal, próximo pc, ou seja pc + 1 
-				else begin	
-					next_temp = pc_in + 1;
-				end
-			end 
-		end
+		JR: begin
+			next_address = jr; 
+		end 
+		NPC: begin
+			next_address = pc_1; 
+		end 
+		JUMP: begin 
+			next_address = jump_address; 
+		end 
+		BRANCH: begin
+			next_address = branch_address; 
+		end 
+		HALT: begin
+			next_address = pc; 
+		end 
+	endcase
+end	
 
-always @(*) begin
-	// se for reset, zera o pc
-	if(reset) begin
-		next_address = 0;
-	end
-	// se não coloca o prox valor se pc na saída
-	else begin  
-		next_address = next_temp;
-	end
-end 	
-endmodule 
+endmodule	 
+
+
